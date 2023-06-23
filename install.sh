189974d4c1a32e21742b88eff2fd79bcb098da2e
Harsh <harshwardhankumar18@gmail.com>
2023-06-23 13:20:48 +0530

    ..

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+# Variables ~
+cdr="$HOME/.config"
+hc="$HOME/harbs/config"
+hm="$HOME/harbs/misc"
+usname=$(whoami)
+
+# Installing Packages ~
+file="$hm/prog.csv"
+for i in $(awk -F',' '{print $2}' "$file")
+do
+    sudo pacman -S "$i"
+done
+
+# Welcome ~
+neofetch
+echo "Installation Will Start Now"
+sleep 3s
+
+# Making Directories ~
+mkdir -p $HOME/pix/wall 
+
+# Copying Files ~
+cp -r $hc/suckless $cdr/
+cp -r $hc/sxhkd $cdr/
+cp $hm/wall.png $HOME/pix/wall/
+
+# Creating Color Themes
+wal -i $HOME/pix/wall/wall.png
+sed -i 'N;$!P;D' $HOME/.cache/wal/colors-wal-dwm.h
+
+# Changing Username in Config Files ~
+progc=("dwm" "st" "dmenu")
+for i in "${progc[@]}"
+do
+	sed -i "s/\username/$usname/" $HOME/.config/suckless/$i/config.def.h
+done
+
+# Installing Suckless Utilities ~
+progi=("dwm" "st" "dmenu" "slstatus" "slock")
+for i in "${progi[@]}"
+do
+	cd $cdr/suckless/$i && sudo cp config.def.h config.h && sudo make install
+done

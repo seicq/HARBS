a8b5e2fccfac2b51ec5e9c655de897f9c3e0e45a
Harsh <harshwardhankumar18@gmail.com>
2023-07-12 19:19:03 +0530

    lil chnges

--- install.sh
+++ install.sh
@@ -1,23 +1,15 @@
+# Source the funcrc
+
 [ -f "$HOME/harbs/static/funcrc" ] && source "$HOME/harbs/static/funcrc"
 
-git clone https://github.com/seicq/boiled-rice $HOME/boiled-rice
-
-reap -p "Do you use artix ? (Y/n)" choice
-case $choice in
-	Y)
-		artix;;
-	n)
-		echo " "
-esac
+# Functions must be run in this order
 
 mirrors
 install_yay
 install_base
 mk_dirs
-cp_files
 install_wm
 install_zsh
 install_brillo
 graphic_driver
-srvces
 clean_up
